cb5ed22379794ff031d1578438802de5f2a210ab
Иван Иванов <kovalevdmv@yandex.ru>
2021-05-05 12:25:40 +0300

    modified:   .vs/c_sharp_mssql/v16/.suo 	modified:   c_sharp_mssql/Program.cs 	modified:   c_sharp_mssql/bin/Debug/net5.0/c_sharp_mssql.dll 	modified:   c_sharp_mssql/bin/Debug/net5.0/c_sharp_mssql.pdb 	modified:   c_sharp_mssql/obj/Debug/net5.0/c_sharp_mssql.csprojAssemblyReference.cache 	modified:   c_sharp_mssql/obj/Debug/net5.0/c_sharp_mssql.dll 	modified:   c_sharp_mssql/obj/Debug/net5.0/c_sharp_mssql.pdb

--- c_sharp_mssql/Program.cs
+++ c_sharp_mssql/Program.cs
@@ -69,19 +69,21 @@ namespace c_sharp_mssql
 
                     try
                     {
-                        using (var sr = new StreamReader("1s.log"))
+                        using (var sr = new StreamReader("1s_big.log"))
                         {
                             string line;
                             int count = 0;
                             int ind = -1;
                             string line_all = "";
-                                
+
                             string[] buf = new string[2]; // буфер на две строки, чтобы можно было узнать что в следующей строке
+                            int count_line_commnd = 1;
                             while ((line = sr.ReadLine()) != null)
                             {
                                 ind++;
 
-                                if (ind > 0) { // для последующий, сместить строки в буфере вверх и поместить в него новую строку
+                                if (ind > 0)
+                                { // для последующий, сместить строки в буфере вверх и поместить в него новую строку
                                     if (ind > 1)
                                         buf[0] = buf[1];
                                     buf[1] = line;
@@ -97,25 +99,34 @@ namespace c_sharp_mssql
                                 if ((buf[1].ToString().Split(",").Length < 2)) continue;
 
                                 line_arr = line_all.Split(",");
-                                if (!line_all.Contains("TLOCK")) { line_all = ""; continue; }
-                                
-                                
-                                command.CommandText = @"insert into Myfiles (id,time,event1s,SessionID,Usr,Regions,Locks,WaitConnections,Context) values (@id,@time,@event1s,@SessionID,@Usr,@Regions,@Locks,@WaitConnections,@Context);";
-                                command.Parameters.Clear();
-                                command.Parameters.AddWithValue("@id", ind);
-                                command.Parameters.AddWithValue("@time", line_arr[(int)LINE.time]);
-                                command.Parameters.AddWithValue("@event1s", line_arr[(int)LINE.event1s]);
-                                command.Parameters.AddWithValue("@SessionID", line_arr[(int)LINE.SessionID]);
-                                command.Parameters.AddWithValue("@Usr", line_arr[(int)LINE.Usr]);
-                                command.Parameters.AddWithValue("@Regions", getProp(line_all, "Regions"));
-                                command.Parameters.AddWithValue("@Locks", getProp(line_all, "Locks"));
-                                command.Parameters.AddWithValue("@WaitConnections", getProp(line_all, "WaitConnections"));
-                                command.Parameters.AddWithValue("@Context", getProp(line_all,"Context"));
-                                line_all = "";
-                                command.ExecuteNonQuery();
 
-                              count++;
+                                if (!line_all.Contains("TLOCK")) { line_all = ""; continue; }
+
+
+                                command.CommandText += String.Format("insert into Myfiles (id,time,event1s,SessionID,Usr,Regions,Locks,WaitConnections,Context) " +
+                                    "values (@id{0},@time{0},@event1s{0},@SessionID{0},@Usr{0},@Regions{0},@Locks{0},@WaitConnections{0},@Context{0});", count_line_commnd);
                                 
+                                command.Parameters.AddWithValue(String.Format("@id{0}", count_line_commnd), ind);
+                                command.Parameters.AddWithValue(String.Format("@time{0}", count_line_commnd), line_arr[(int)LINE.time]);
+                                command.Parameters.AddWithValue(String.Format("@event1s{0}", count_line_commnd), line_arr[(int)LINE.event1s]);
+                                command.Parameters.AddWithValue(String.Format("@SessionID{0}", count_line_commnd), line_arr[(int)LINE.SessionID]);
+                                command.Parameters.AddWithValue(String.Format("@Usr{0}", count_line_commnd), line_arr[(int)LINE.Usr]);
+                                command.Parameters.AddWithValue(String.Format("@Regions{0}", count_line_commnd), getProp(line_all, "Regions"));
+                                command.Parameters.AddWithValue(String.Format("@Locks{0}", count_line_commnd), getProp(line_all, "Locks"));
+                                command.Parameters.AddWithValue(String.Format("@WaitConnections{0}", count_line_commnd), getProp(line_all, "WaitConnections"));
+                                command.Parameters.AddWithValue(String.Format("@Context{0}", count_line_commnd), getProp(line_all, "Context"));
+                                if (count_line_commnd == 50)
+                                {
+                                    command.ExecuteNonQuery();
+                                    command.Parameters.Clear();
+                                    command.CommandText = "";
+                                    count_line_commnd = 1;
+                                }
+                                count_line_commnd++;
+                                line_all = "";
+
+                                count++;
+
                                 if (count == 1000)
                                 {
                                     count = 0;
